ae226090c47d09bb9172e1b6259d35fe9777a8f6
root <root@CyberKannibal.CyberKa>
2024-08-17 16:45:58 -0400

    initial commit

--- sw.js
+++ sw.js
@@ -2,10 +2,8 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-
-// Substitua pelo nome correto da página de fallback offline
-const offlineFallbackPage = "offlinePages.html";
+const CACHE = "pagoda-offline-page";
+const offlineFallbackPage = "offlinePages.html"; // Substitua pelo nome correto da página offline
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -37,7 +35,6 @@ self.addEventListener('fetch', (event) => {
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
